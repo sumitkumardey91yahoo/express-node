33d356da47d4eb11dd4d0268313018416c3ee3b3
sumitkumardey91 <sumit.kumar@mobiotics.com>
2020-02-20 18:34:54 +0530

    mongodb

--- mongo/index.js
+++ mongo/index.js
@@ -1,21 +1,53 @@
-const MongoClient = require('mongodb').MongoClient;
-const mongoose = require('mongoose');
+const Mongo = require('mongodb');
+
+const express = require('express');
+
+const bodyParser = require('body-parser')
+
+
+const app = express();
+
+app.use(bodyParser.json())
+
+const MongoClient =  Mongo.MongoClient;
+
 const uri = "mongodb+srv://sumitkumardey91:sumit@1991@cluster0-u9py9.mongodb.net/test?retryWrites=true&w=majority";
 
-
-// mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }).then(() => {
-//   console.log("connect")
-// }).catch(() => {
-//   console.log("pl")
-// })
-
-
-
 const client = new MongoClient(uri, { useUnifiedTopology: true,  useNewUrlParser: true } );
+
+
 client.connect(err => {
   const db = client.db("mydb")
 
-  console.log(db)
+
+  app.get('/', (req, res) => {
+    db.collection('customers').find().toArray(function (err, result) {
+      if (err) throw err
+      console.log(result)
+      res.send(result)
+    })
+  });
+
+  app.post('/', (req, res) => {
+   
+    const data = req.body;
+    db.collection('customers').insert(data);
+    res.send("insert successfully");
+
+  });
+   app.delete('/', (req, res) => {
+    const data = req.body;
+    db.collection('customers').remove(data);
+    res.send("delete successfully");
+  });
+ 
+  app.put('/', (req, res) => {
+    const city = req.body.city;
+    db.collection('customers').update({ "city": "BENGALURU" }, {$set: { "city": city}})
+    res.send("update successfully");
+  });
+
+  // console.log(db)
   // db.collection('customers').insert({name: 'delhi'})
   // client.close();
 
@@ -38,9 +70,17 @@ client.connect(err => {
   //db.collection('customers').remove({address: 'bangalore'})
 
 
-  db.collection('customers').find().toArray(function (err, result) {
-        if (err) throw err
-        console.log(result)
-})
+//   db.collection('customers').find().toArray(function (err, result) {
+//         if (err) throw err
+//         console.log(result)
+// })
 
 });
+
+
+
+app.listen(8000, (req) => {
+
+  console.log("started")
+
+})
